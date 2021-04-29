f0408a08c45f2d1bc4e28228e60f9ba2977bb26d
artemats <artem.matseruk@gmail.com>
2021-04-29 15:45:58 +0300

    fix header on iphone

--- src/js/scroll/detectHeaderStatus.js
+++ src/js/scroll/detectHeaderStatus.js
@@ -2,18 +2,22 @@ export function detectHeaderStatus(direction, section) {
 
     const header = document.querySelector('.header');
 
-    /// toggle header position ///
-    if(direction === 'down') {
-        header.classList.add('hidden');
-    } else {
-        header.classList.remove('hidden');
-    }
+    if(window.window > 1200) {
 
-    /// for first section ///
-    if(section.getAttribute('id') === 'section-0') {
-        header.classList.remove('smart');
-    } else {
-        header.classList.add('smart');
+        /// toggle header position ///
+        if (direction === 'down') {
+            header.classList.add('hidden');
+        } else {
+            header.classList.remove('hidden');
+        }
+
+        /// for first section ///
+        if (section.getAttribute('id') === 'section-0') {
+            header.classList.remove('smart');
+        } else {
+            header.classList.add('smart');
+        }
+
     }
 
     // if(window.innerWidth <= 1200) {
