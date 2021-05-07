29d3fcb1935b25c810af32343b80f649c6005751
artemats <artem.matseruk@gmail.com>
2021-05-07 11:28:06 +0300

    fix chart animation

--- src/js/common/initChartAnimation.js
+++ src/js/common/initChartAnimation.js
@@ -1,4 +1,5 @@
-import gsap from 'gsap';
+import gsap, { Power3 } from 'gsap';
+import BezierEasing from 'bezier-easing';
 
 const chart = document.querySelector('.chart');
 const messages = document.querySelectorAll('.chart-message');
@@ -7,22 +8,74 @@ const status = document.querySelector('.chart-status');
 export const initChartAnimation = () => {
 
     if(!chart.classList.contains('active')) {
-        gsap.fromTo(messages, {
-            position: 'absolute',
-            opacity: 0,
-        }, {
-            delay: 2,
-            position: 'relative',
-            opacity: 1,
-            duration: 1,
-            stagger: 1.5,
-            onComplete: () => {
-                status.classList.remove('active');
-            }
-        });
+
+        setTimeout(() => showMessage(messages[0]), 1000);
+
+        // gsap.fromTo(messages, {
+        //     position: 'absolute',
+        //     opacity: 0,
+        //     scale: 0.9,
+        // }, {
+        //     delay: 2,
+        //     position: 'relative',
+        //     opacity: 1,
+        //     duration: 0.8,
+        //     stagger: 1.5,
+        //     scale: 1,
+        //     ease: Power3.easeInOut,
+        //     onComplete: () => {
+        //         status.classList.remove('active');
+        //     }
+        // });
 
         chart.classList.add('active');
     }
+};
+
+const showMessage = (message) => {
+    status.classList.add('active');
+
+    gsap.fromTo(message, {
+        position: 'absolute',
+        opacity: 0,
+        scale: 0.9,
+    }, {
+        delay: 1.5,
+        position: 'relative',
+        opacity: 1,
+        duration: 0.6,
+        scale: 1,
+        ease: BezierEasing(0.68, -0.39, 0.27, 1.39),
+        onComplete: () => {
+            showMessage2(messages[1]);
+        }
+    });
+
+    setTimeout(() => {
+        status.classList.remove('active');
+    }, 1500);
+};
+
+const showMessage2 = (message) => {
+    status.classList.add('active');
+
+    gsap.fromTo(message, {
+        position: 'absolute',
+        opacity: 0,
+        scale: 0.9,
+    }, {
+        delay: 1.5,
+        position: 'relative',
+        opacity: 1,
+        duration: 0.6,
+        scale: 1,
+        ease: BezierEasing(0.68, -0.39, 0.27, 1.39),
+    });
+
+    setTimeout(() => {
+        status.classList.add('finished');
+        status.classList.remove('active');
+    }, 1600);
 };
 
 // export const stopChartAnimation = () => {
